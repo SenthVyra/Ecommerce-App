7173356fd2501a0518f639496819004bab2da904
SenthVyra <88665212+SenthVyra@users.noreply.github.com>
2025-01-22 12:01:33 +0530

    image success

---
+++ resources/views/livewire/item-card.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-gray-300 shadow-sm rounded-lg">
+    <div>
+        <img src="{{ asset('images/3.jpg') }}" alt="product-image" class="rounded object-cover w-full h-96">
+    </div>
+</div>
